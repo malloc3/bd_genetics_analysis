4c837cfb543b987bd53f919ffd7c50802c97ea08
Cannon Mallory <mallorycannon15@gmail.com>
2025-12-24 12:35:21 -0800

    remove the alignment steps to see if the otehr step works

--- bowtie_alignment/bowtie_run_script.sh
+++ bowtie_alignment/bowtie_run_script.sh
@@ -94,15 +94,15 @@ conda run -n bowtie_environ  bowtie2-build "$reference_genome" "$reference_genom
 echo "Reference genome indexing complete!"
 echo "=========================================="
 
-echo "Lets align things!"
+#echo "Lets align things!"
 
-for fq in "${fastq_files[@]}"; do
-    start_time=$(date '+%Y-%m-%d %H:%M:%S')
-    echo "[$start_time] Aligning ${fastq_files[fq]} to $reference_genome_name "
-    conda run -n bowtie_environ bowtie2 -x "$reference_genome_name" -U "${fastq_files[fq]}" -p 24 #Need to figure out how to set the outputs
-    end_time=$(date '+%Y-%m-%d %H:%M:%S')
-    echo "[$end_time] Alignment complete!"
-    echo "=========================================="
-done
+#for fq in "${fastq_files[@]}"; do
+#    start_time=$(date '+%Y-%m-%d %H:%M:%S')
+#    echo "[$start_time] Aligning ${fastq_files[fq]} to $reference_genome_name "
+#    conda run -n bowtie_environ bowtie2 -x "$reference_genome_name" -U "${fastq_files[fq]}" -p 24 #Need to figure out how to set the #outputs
+#    end_time=$(date '+%Y-%m-%d %H:%M:%S')
+#    echo "[$end_time] Alignment complete!"
+#    echo "=========================================="
+#done
 
 echo "end run"
