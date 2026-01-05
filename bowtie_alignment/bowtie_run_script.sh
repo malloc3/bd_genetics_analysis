0fd09b172cba5c9df52bf1518f0639e8fe8794da
Cannon Mallory <mallorycannon15@gmail.com>
2026-01-04 17:32:50 -0800

    Updating script to run with the v1.1 genome original genome I think

--- bowtie_alignment/bowtie_run_script.sh
+++ bowtie_alignment/bowtie_run_script.sh
@@ -5,10 +5,10 @@
 #This script is used to align the unpaired fastq files that I downloaded from NCBI.
 
 #Add the complete path to the reference genome you want to use!
-reference_genome="$SLURM_SUBMIT_DIR""/fast_q_files/Reference_genomes/Near_complete_2025_non_ncbi_fasta/CMM_BatrDend_JEL423_V3.genome.fasta"
+reference_genome="$SLURM_SUBMIT_DIR""/fast_q_files/Reference_genomes/v1.1/ncbi_dataset/data/GCA_00203795.2/GCA_000203795.2_v1.1"
 
 #this is the name of the index files that will be created for this reference genome
-reference_genome_name="Near_complete_bd"
+reference_genome_name="v1.1_genome_original_bd_genome_I_think"
 
 # update this location for where your files are located
 fastq_file_location="$SLURM_SUBMIT_DIR""/fast_q_files/unpaired/"
