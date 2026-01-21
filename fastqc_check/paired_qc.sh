dc709581bc280cb85871e72fc4a715481af29a55
Cannon Mallory <mallorycannon15@gmail.com>
2026-01-20 20:41:17 -0800

    Changing folders to have sub folders of unpaired and paired for easier viewing later

--- fastqc_check/paired_qc.sh
+++ fastqc_check/paired_qc.sh
@@ -7,7 +7,8 @@
 # FastQC run settings
 memory_per_file=$1
 number_of_threads=$2
-run_results_folder=$3
+main_results_folder=$3
+run_results_folder="./""$main_results_folder""/paired"
 
 # update this location for where your files are located
 fastq_file_location="$SLURM_SUBMIT_DIR""/../bowtie_alignment/fast_q_files/paired/"
