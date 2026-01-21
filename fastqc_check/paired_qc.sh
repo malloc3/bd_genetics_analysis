42ec7ba6fc27ca844cfd18838024d8257754336f
Cannon Mallory <mallorycannon15@gmail.com>
2026-01-21 12:22:18 -0800

    lkdjfg

--- fastqc_check/paired_qc.sh
+++ fastqc_check/paired_qc.sh
@@ -18,7 +18,7 @@ mkdir "$run_results_folder"
 fastq_file_location="$SLURM_SUBMIT_DIR""/../bowtie_alignment/fast_q_files/paired"
 
 # update this to point to the csv file that contains the names of fastq files you want to analyze
-fastq_files_csv_path="$SLURM_SUBMIT_DIR""/paired_fastq_names-2.csv"   #Should update this to only lookq locally
+fastq_files_csv_path="$SLURM_SUBMIT_DIR""/paired_names.txt"   #Should update this to only lookq locally
 
 
 #Uf the csv exists then then makes a list of all the names
