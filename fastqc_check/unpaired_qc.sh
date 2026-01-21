3dbf3257a717164409e2355c680009d411ea1298
Cannon Mallory <mallorycannon15@gmail.com>
2026-01-20 20:04:24 -0800

    adding thign ak;sjgh

--- fastqc_check/unpaired_qc.sh
+++ fastqc_check/unpaired_qc.sh
@@ -70,24 +70,17 @@ fi
 
 
 
-# Get todays date.   
-todays_date=$(date '+%Y-%m-%d_%H-%M-00')
-
-#Make a directoryt o save settings files and setting information
-run_results="fast_qc_results""$todays_date"
-mkdir "./""$run_results"
-
 # 2. Make a file for output information and settins to be recorded
-log_doc_name="fast_qc_settings.txt"
-log_doc_full_path="$run_results""/""$log_doc_name"
+log_doc_name="unpaired_fast_qc_settings.txt"
+log_doc_full_path="$RUN_RESULTS""/""$log_doc_name"
 touch "$log_doc_full_path"
 echo "FastQC Log" >> "$log_doc_full_path"
 echo "date: ""$todays_date" >> "$log_doc_full_path"
 echo "Author: Cannon Mallory" >> "$log_doc_full_path"
 echo "Ran by SLURM Script on UCSB Pod" >> "$log_doc_full_path"
 echo "Results shoudl be located here: ""$run_results" >> "$log_doc_full_path"
-echo "Memory allocated to run each file: ""$memory_per_file" >> "$log_doc_full_path"
-echo "Number of threads per chunk of files ran: ""$number_of_threads" >> "$log_doc_full_path"
+echo "Memory allocated to run each file: ""$MEMORY_PER_FILE" >> "$log_doc_full_path"
+echo "Number of threads per chunk of files ran: ""$NUMBER_THREADS" >> "$log_doc_full_path"
 
 
 # 3. write any needed info to that file of note
@@ -98,7 +91,7 @@ echo "Number of threads per chunk of files ran: ""$number_of_threads" >> "$log_d
 # Looks through the fastq files list and runs the fastqc on each of thsoe files
 # Then saves it to the run_results folder
 echo "Lets run rast qc on the files"
-./FastQC/fastqc ${fastq_files[@]} --outdir="./""$run_results" --memory=$memory_per_file -t=$number_of_threads
+#./FastQC/fastqc ${fastq_files[@]} --outdir="./""$run_results" --memory=$memory_per_file -t=$NUMBER_THREADS
 #for file in "${fastq_files[@]}"; do
 #    ./FastQC/fastqc "$file" --outdir="./""$run_results" --memory=$memory_per_file -t=$number_of_threads
 #done
