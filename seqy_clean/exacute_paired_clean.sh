db463ae85311d7c152803857ba80776445caae22
Cannon Mallory <mallorycannon15@gmail.com>
2026-01-27 15:05:17 -0800

    fixing otput prefex thing

--- seqy_clean/exacute_paired_clean.sh
+++ seqy_clean/exacute_paired_clean.sh
@@ -129,19 +129,16 @@ for ((i=0; i<num_files; i+=2)); do
     full_file_path_1="$fastq_file_location""/""$file_name_1"
     full_file_path_2="$fastq_file_location""/""$file_name_2"
 
-    output_prefex="todays_date""$shortened_name""_cleaned_"
-    output_folder="$results_folder/$output_folder_name"
+    output_file_prefex="todays_date""$shortened_name""_cleaned_"
+    output_folder="$results_folder/$shortened_name"
+    output_prefex_full="$output_folder""/""$output_file_prefex"
     mkdir "$output_folder"
 
     echo "Running: ""$file_name_1"" and ""$file_name_2"
     echo "Final items should be saved with output_prex: $output_prefex and in folder $output_folder"
-    ./seqyclean/bin/seqyclean -qual -dup -detrep -at 0.75 -v $vector_file -1 $full_file_path_1 -2 $full_file_path_2    -o output_prefex #add options etc
 
-    #Moves all the folders to the results folder so we don't have to get out main folder all crowded up.
-    shopt -s nullglob
-    mv *output_prefex* "$output_folder"
-    shopt -u nullglob 
-
+    # Can add additional options
+    ./seqyclean/bin/seqyclean -qual -dup -detrep -at 0.75 -v $vector_file -1 $full_file_path_1 -2 $full_file_path_2    -o "$output_prefex" 
 done
 
 
