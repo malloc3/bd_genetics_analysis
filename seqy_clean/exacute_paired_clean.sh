88ce6cc3f2219624aed9b94578ef6beed0679a36
Cannon Mallory <mallorycannon15@gmail.com>
2026-01-27 18:34:52 -0800

    Please commit right

--- seqy_clean/exacute_paired_clean.sh
+++ seqy_clean/exacute_paired_clean.sh
@@ -4,7 +4,6 @@
 
 #This script is to run the fastqc run script to check the quality scores of each sequence.
 
-
 #/home/cmallory/Genetic_analysis/bd_genetics_analysis/bowtie_alignment/fast_q_files/paired/CJB4_5720-IR-1_GCGTAGTA-CTAGTCGA_S1_R_0011.fastq
 
 todays_date=$1
