b5c95dc74598a92759229cc08353c4e4e192483c
Cannon Mallory <mallorycannon15@gmail.com>
2026-07-21 18:11:33 -0700

    Addition of sam tools slrm folder with some of the sub scripts needed for run my sam tools.  Currently only added the idxstats folder that will run the Stamyp and Seqycleaned files.  I will test these on the cluster

---
+++ sam_tools/sam_tools_slrm/READ_ME.txt
@@ -0,0 +1,5 @@
+This sub folder is where I will keep all of my scripts and associated files that will run the slurm jobs and outputs from those slurm jobs on all my sequences.
+
+On the cluster samtools and bcf tools appear to be operational.  You must remmeber to give the full path to the exacutable files though!   Currently the samtools exacutables are not directly in the system PATH which is fine for me.  Keeps versioning a little easier.  Just be explicit!
+
+
---
+++ sam_tools/sam_tools_slrm/idxstats/run_idxstats.sh
@@ -0,0 +1,119 @@
+
+reference_genome_path=$1
+sam_files_text_path=$2
+save_folder_name_prefix=$3
+num_tasks=$4
+num_nodes=$5
+
+
+# maps the text files that we will be using into a single array for use later
+if [ -f $sam_files_text_path ]; then
+    mapfile -t sam_file_names < <(awk -F',' '{for(i=1;i<=NF;i++) print $i}' $sam_files_text_path)
+else
+    echo "File path $sam_files_text_path does not exist"; exit 1
+fi
+
+
+
+echo "============================"
+echo "Looking for reference genome..."
+
+if [ -f "$reference_genome_path" ]; then
+    echo "Reference Genome found:"
+    echo "	$reference_genome_path"
+else
+    echo "Reference genome not found."
+    echo "	$reference_genome_path"
+    exit 1
+fi
+
+if [ -r "$reference_genome_path" ]; then
+    echo "Refrerence genomes is readable"
+else
+    echo "reference genome is not readable"; exit 1
+fi
+
+
+echo "============================"
+
+
+
+# Checks that all of our files exist!
+echo "Checking that all sam files exist"
+sam_files=()  # Creates a list of the FULL file paths to each and every sample
+
+all_files_exist=true
+non_existing_files=()
+non_readable_fast_q_files=()
+
+for i in "${!sam_file_names[@]}"; do
+    full_path="${sam_file_names[$i]}"
+    if [ -f "$full_path" ]; then
+        if [ -r $full_path ]; then
+            sam_files+=("$full_path")
+        else
+            all_files_exist=false
+            non_readable_fast_q_files+=("$full_path")
+        fi
+    else
+	    all_files_exist=false
+	    non_existing_files+=("$full_path")
+    fi
+done
+
+if $all_files_exist; then
+    echo "All fastq files exist and are readable!"
+    echo "============================"
+else
+    echo "The following files do not exist:"
+    for i in "${!non_existing_files[@]}"; do
+	    echo "	${non_existing_files[$i]}"
+    done
+    echo "The following files are not readable:"
+    for i in "${!non_readable_fast_q_files[@]}"; do
+	    echo "	${non_readable_fast_q_files[$i]}"
+    done
+    echo "Run Failed"
+    exit 1 
+fi
+
+
+
+
+# Make the output folder
+todays_date=$(date '+%Y-%m-%d_%H-%M-00')
+#Make a directoryt o save settings files and setting information
+base_save_folder="./""$save_folder_name_prefix""idxstats""$todays_date"
+mkdir "$base_save_folder"
+
+
+today_day=$(date '+%Y-%m-%d')
+
+
+echo "Okay lets deduplicate some things!"
+
+for i in "${!sam_file_names[@]}"; do
+    sam_file_path="${sam_file_names[$i]}"
+
+    sam_file_name=${sam_file_path##*/}  # This gets only the file name but includes the file type extention
+    only_file_name=${sam_file_name%.sam} # This drops the file type extention as well!
+
+    output_name="samtools-idxstats""$today_day""--""$only_file_name"".txt"  # should give me just the file name from the whole path
+    echo "Input file information"
+    echo "$sam_file_path"
+    echo "$sam_file_name"
+
+    
+
+    sam_results_save_file="./""$run_results_folder""/""$output_name"                      # Sets the save location of the deduplication
+
+    echo ""
+    echo ""
+    echo "Output  File information"
+    echo "$sam_results_save_file"
+
+    /home/cmallory/Genetic_analysis/bd_genetics_analysis/sam_tools/sam_bin/bin/samtools idxstats -X $sam_file_path > $sam_results_save_file
+
+    echo "=================="
+
+done
